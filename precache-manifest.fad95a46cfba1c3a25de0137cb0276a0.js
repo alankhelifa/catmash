78da7d24a7f7649f2c6ba31ddecc2eef062ec1fa
Alan Khelifa <alan@khelifa.dev>
2020-01-09 18:14:08 +0100

    Updates

---
+++ precache-manifest.fad95a46cfba1c3a25de0137cb0276a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef253b3b66f817df4e7aebe98844e94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5a836a21cd2bfb4f0a6",
+    "url": "/static/js/2.4800f835.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4800f835.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b56bd6952f7077b04a75",
+    "url": "/static/js/main.27639507.chunk.js"
+  },
+  {
+    "revision": "ca1abf36a8c32a9fd411",
+    "url": "/static/js/runtime-main.2cb8b5d3.js"
+  }
+]);
